586da858f69ba8f797f9393e4cf4225544bfa86f
kursivee <kursivee.dev@gmail.com>
2020-01-23 22:47:31 -0500

    [Refactor] Remove unneeded annotations for viewmodelkey

--- lib/src/main/java/com/kursivee/lib/presentation/viewmodel/ViewModelKey.kt
+++ lib/src/main/java/com/kursivee/lib/presentation/viewmodel/ViewModelKey.kt
@@ -4,6 +4,6 @@ import androidx.lifecycle.ViewModel
 import dagger.MapKey
 import kotlin.reflect.KClass
 
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Target(AnnotationTarget.FUNCTION)
 @MapKey
 annotation class ViewModelKey(val value: KClass<out ViewModel>)
